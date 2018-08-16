2f878f4866f4ccd499e32889f572204615740794
Gustavo Rosa <gth.rosa@uol.com.br>
2018-08-16 15:49:33 -0300

    Adding a data module to post process data.

--- examples/load_preprocess_data.py
+++ examples/load_preprocess_data.py
@@ -1,15 +1,24 @@
+import feelyng.stream.data as d
 import feelyng.stream.loader as l
 import feelyng.stream.preprocess as p
 
-# Loads an input data
-data = l.load_csv('data/twitter_en.csv')
+import pandas as pd
 
-# Creates a preprocessing pipeline
+# Loads an input .csv
+csv = l.load_csv('data/twitter_en.csv')
+
+# Creates a pre-processing pipeline
 pipe = p.pipeline(
-    p.lower_case
+    p.lower_case,
+    p.valid_char
 )
 
-# Apply pipeline to data
-data['text'] = data['text'].apply(lambda x: pipe(x))
+# Transforming dataframe into samples and labels
+X = csv['text']
+Y = csv['sentiment']
 
-print(data['text'])
+# Applying pre-processing pipeline to X
+X = X.apply(lambda x: pipe(x))
+
+# Splitting data into training and testing sets
+X_train, X_test, Y_train, Y_test = d.split_data(X, Y, 0.5)
---
+++ feelyng/stream/data.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+import feelyng.utils.logging as l
+from sklearn.model_selection import train_test_split
+
+
+logger = l.get_logger(__name__)
+
+
+def split_data(X, Y, split_size=0.5, random_state=42):
+    """Splits X, Y (samples, labels) data into training and testing sets.
+
+    Args:
+        X (): Tokenized data.
+        Y (numpy.array | pandas.DataFrame): Numpy array (categorical labels) or panda's dataframe column.
+        split_size (float): The proportion of test sets.
+        random_state (int): Random integer to provide a random state to splitter.
+
+    Returns:
+        X and Y training, testing sets.
+
+    """
+
+    try:
+        Y = pd.get_dummies(Y).values
+    except Exception as e:
+        logger.warn(e)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=split_size, random_state=42)
+
+    return X_train, X_test, Y_train, Y_test
