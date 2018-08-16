644b615da1204f8182358c2000b539a9618bfac9
Gustavo Rosa <gth.rosa@uol.com.br>
2018-08-16 11:21:03 -0300

    Adding logging utilities and first set of loading functions.

---
+++ feelyng/stream/loader.py
@@ -0,0 +1,26 @@
+import sys
+
+import feelyng.utils.logging as l
+import pandas as pd
+
+logger = l.get_logger(__name__)
+
+
+def load_csv(csv_path):
+    """Loads a CSV file into a dataframe object.
+
+    Args:
+        csv_path (str): a string holding the .csv's path
+
+    Returns:
+        A Panda's dataframe.
+    """
+    try:
+        # Tries to read .csv file into a dataframe
+        data = pd.read_csv(csv_path)
+    except FileNotFoundError as e:
+        # If file is not found, handle the exception and exit
+        logger.error('Failed to open file ' + csv_path)
+        raise Exception(e)
+
+    return data
