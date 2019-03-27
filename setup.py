fe8dfc5545f74d0aabbb1fbaba613fa9286ade38
Gustavo Rosa <gth.rosa@uol.com.br>
2019-03-27 15:51:18 -0300

    Fixing requirements.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='nalp',
                         'pytest>=3.2.3',
                         'scikit-learn>=0.19.2',
                         'scipy>=1.1.0',
-                        'tensorflow>=1.13.1'
+                        'tensorflow>=2.0.0-alpha0'
                         ],
       extras_require={
           'tests': ['coverage',
