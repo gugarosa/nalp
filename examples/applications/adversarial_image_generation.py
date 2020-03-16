b507ba0580834d13417a1ef0912a80b18b681328
Gustavo Rosa <gth.rosa@uol.com.br>
2020-03-16 18:20:32 -0300

    Adding the possibility to plot a mosaic instead of only one image.

--- examples/applications/adversarial_image_generation.py
+++ examples/applications/adversarial_image_generation.py
@@ -7,6 +7,9 @@ from nalp.models.adversarial.gan import GAN
 # to use the same data, classes and parameters
 # as the pre-trained network
 
+# Defining a constant to hold the amount of samples to generate
+N_SAMPLES = 16
+
 # Defining a constant to hold the input noise dimension
 N_NOISE = 100
 
@@ -20,16 +23,21 @@ gan = GAN(gen_input=N_NOISE, gen_output=N_FEATURES, alpha=0.01)
 gan.load_weights('trained/gan').expect_partial()
 
 # Creating a noise tensor for further sampling
-z = tf.random.normal([1, N_NOISE])
+z = tf.random.normal([16, N_NOISE])
 
 # Sampling an artificial image
-sampled_image = tf.reshape(gan.sample(z), (28, 28))
+sampled_images = tf.reshape(gan.sample(z), (N_SAMPLES, 28, 28))
 
 # Creating a pyplot figure
-fig = plt.figure()
+fig = plt.figure(figsize=(4,4))
 
-# Plotting the image to the figure
-plt.imshow(sampled_image * 127.5 + 127.5, cmap='gray')
+# For every possible generated image
+for i in range(sampled_images.shape[0]):
+    # Defines the subplot
+    plt.subplot(4, 4, i+1)
+
+    # Plots the image to the figure
+    plt.imshow(sampled_images[i, :, :] * 127.5 + 127.5, cmap='gray')
 
 # Showing the plot
 plt.show()
