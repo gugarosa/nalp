ee973509339c71016225ebe2ad670d2620846fc1
Gustavo Rosa <gth.rosa@uol.com.br>
2020-04-01 17:11:56 -0300

    Still need to perform the multi-headed attention.

--- nalp/models/layers/relational_memory_cell.py
+++ nalp/models/layers/relational_memory_cell.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import AbstractRNNCell, Dense
+from tensorflow.keras.layers import AbstractRNNCell, Dense, LayerNormalization
 from tensorflow.python.keras.utils import tf_utils
 from tensorflow.python.keras import activations
 from tensorflow.python.keras import constraints
@@ -39,6 +39,10 @@ class RelationalMemoryCell(AbstractRNNCell):
         self.activation = math_ops.tanh
         self.n_gates = 2 * self.memory_size
 
+        self.mlp_layers = [Dense(self.memory_size, activation='relu') for _ in range(n_layers)]
+        self.norm1 = LayerNormalization()
+        self.norm2 = LayerNormalization()
+
         self.input_projection = Dense(self.memory_size)
 
 
@@ -70,7 +74,18 @@ class RelationalMemoryCell(AbstractRNNCell):
 
         self.built = True
 
+    def _multi_head_attention(self, inputs, memory):
+        return memory
+
     def _attend_over_memory(self, inputs, memory):
+        for _ in range(self.n_blocks):
+            att_memory = self._multi_head_attention(inputs, memory)
+            memory = self.norm1(att_memory + memory)
+            mlp_memory = memory
+            for mlp in self.mlp_layers:
+                mlp_memory = mlp(mlp_memory)
+            memory = self.norm2(memory + mlp_memory)
+
         return memory
 
     def call(self, inputs, states):
@@ -79,31 +94,30 @@ class RelationalMemoryCell(AbstractRNNCell):
         """
 
         h_prev, m_prev = states
+
         inputs = self.input_projection(inputs)
 
-        inputs_i = inputs
         inputs_f = inputs
+        inputs_i = inputs
 
-        k_i, k_f = array_ops.split(self.kernel, num_or_size_splits=2, axis=1)
-        rk_i, rk_f = array_ops.split(self.recurrent_kernel, num_or_size_splits=2, axis=1)
+        k_f, k_i = array_ops.split(self.kernel, num_or_size_splits=2, axis=1)
+        rk_f, rk_i = array_ops.split(self.recurrent_kernel, num_or_size_splits=2, axis=1)
 
-        x_i = K.dot(inputs_i, k_i)
         x_f = K.dot(inputs_f, k_f)
+        x_i = K.dot(inputs_i, k_i)
 
-        x_i += K.dot(h_prev, rk_i)
         x_f += K.dot(h_prev, rk_f)
+        x_i += K.dot(h_prev, rk_i)
 
+        b_f, b_i = array_ops.split(self.bias, num_or_size_splits=2, axis=0)
 
-        b_i, b_f = array_ops.split(self.bias, num_or_size_splits=2, axis=0)
-
-        x_i = K.bias_add(x_i, b_i)
         x_f = K.bias_add(x_f, b_f)
+        x_i = K.bias_add(x_i, b_i)
 
         m = self._attend_over_memory(inputs, m_prev)
 
-        h = math_ops.tanh(m)
-
         m = math_ops.sigmoid(x_f + self.forget_bias) * m_prev + math_ops.sigmoid(x_i) * math_ops.tanh(m)
 
+        h = math_ops.tanh(m)
 
         return h, [h, m]
