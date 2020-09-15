c710280befe061f80a9b4ec6559f47918c9c8fcd
Gustavo Rosa <gth.rosa@uol.com.br>
2020-09-15 14:51:55 -0300

    Bumping version for future release.

--- docs/conf.py
+++ docs/conf.py
@@ -27,10 +27,10 @@ copyright = '2020, Gustavo de Rosa'
 author = 'Gustavo de Rosa'
 
 # The short X.Y version
-version = '2.0.0'
+version = '2.0.1'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='nalp',
-      version='2.0.0',
+      version='2.0.1',
       description='Natural Adversarial Language Processing',
       long_description=long_description,
       long_description_content_type='text/markdown',
